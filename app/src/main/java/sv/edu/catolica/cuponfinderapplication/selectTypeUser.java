94c0b3709370dab9d54c54e6034a3b767e0db6b1
Verónica Marín <veronica.marin@catolica.edu.sv>
2023-11-01 23:42:32 -0600

    Initial commit

---
+++ app/src/main/java/sv/edu/catolica/cuponfinderapplication/selectTypeUser.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.cuponfinderapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class selectTypeUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_type_user);
+    }
+}
